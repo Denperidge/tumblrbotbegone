1ddf95bd9e2a14144a5c299225fad976edd46b7c
Denperidge <denperidge@gmail.com>
2023-09-12 22:17:22 +0200

    Begin work on login redirect & state

--- tumblrlogin/views.py
+++ tumblrlogin/views.py
@@ -1,3 +1,5 @@
+from secrets import token_urlsafe
+
 from django.shortcuts import render, redirect
 from django.http.request import HttpRequest
 from django.conf import settings
@@ -15,8 +17,7 @@ def index(request : HttpRequest):
     if logged_in_as is None:
         return render(request, "tumblrlogin/login.html")
 
-def cb():
-    pass
+
 
 oauth = OAuth()
 tumblr_oauth = oauth.register("tumblr")
@@ -27,16 +28,22 @@ tumblr = WebApplicationClient(
     response_type="code",
     scope=" ".join([]),
     # state is automatically hjandled
-    state="meow",
     redirect_uri=settings.OAUTH_REDIRECT_URL,
 )
-def login(request):
+def login(request: HttpRequest):
     # https://www.tumblr.com/docs/en/api/v2#oauth2authorize---authorization-request
     print(tumblr_oauth)
     print(type(tumblr_oauth))
+    request.session["state"] = token_urlsafe()
+    request.session.save()
     #request()
     return redirect(
-        tumblr.prepare_request_uri("https://www.tumblr.com/oauth2/authorize")
+        tumblr.prepare_request_uri("https://www.tumblr.com/oauth2/authorize", state=request.session.get("state"))
     )
     #return redirect(f"https://www.tumblr.com/oauth2/authorize?client_id=prtjh0DtZ4pbrLtSHQlI3BnZxsTynDobxyORyTxy6CCDRwBLns&response=code&redirect_uri=http://localhost:8000&state=meow")
     #return tumblr_oauth.authorize_redirect(request)
+
+def redirect_callback(request: HttpRequest):
+    req = tumblr.parse_request_uri_response(request.get_full_path(), state=request.session.get("state"))
+    print(req)
+    return ""
